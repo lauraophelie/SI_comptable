00ee9fd4be9ade42f6f067e81fb32ce1946fc5ee
DiamondraAndriam <118435008+DiamondraAndriam@users.noreply.github.com>
2023-04-05 05:37:45 +0300

    update balance

--- inc/balance/fonctions.php
+++ inc/balance/fonctions.php
@@ -1,11 +1,10 @@
 <?php
-    function db_connect() {
+    function dbconnect() {
         $PARAM_hote = 'localhost';
         $PARAM_nom_bd = 'gestion_compta';
         $PARAM_utilisateur = 'gestion_compta';
         $PARAM_mot_passe = 'compta';
         $PARAM_port = '5432';
-    
             try {
                 $connexion = new PDO('pgsql:host='.$PARAM_hote.'.;port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
                 return $connexion;
@@ -14,6 +13,20 @@
                 echo 'Erreur : '.$e-> getMessage().'<br />';
                 echo 'N° : '.$e-> getCode();
             }
+    }
+    function getDebutCompta($id_societe){
+        try {
+            $connexion = dbconnect();
+            $sql = "select date_debut_exercice from comptabilite where societe = :societe order by date_debut_exercice desc limit 1";
+            $stmt = $connexion->prepare($sql);
+            $stmt->bindParam(':societe', $id_societe);
+            $stmt->execute();
+            $resultat = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $resultat[0]["date_debut_exercice"];
+        } catch (PDOException $e){
+            echo "Error: " . $e->getMessage();
+            return false;
+        }
     }
 
     function getAllSolde($debut, $societe){
@@ -32,12 +45,37 @@
         }
     }
 
-    /*function treatmentSolde($listes){
-        //liste ['']
+    function treatmentSolde($listes){
+        //liste [numero,desognation,deb,cred]
         $debit = 0;
         $credit = 0;
-        foreach($listes as $liste){
-
+        $new_liste = array();
+        $j = 0;
+        for($i=0; $i>count($listes);$i++){
+            $new_liste[$j]['numero']=$listes[$i]['numero'];
+            if($listes[$i]['deb']>$listes[$i]['cred']){
+                $new_liste[$j]['mvt']=$listes[$i];
+                $new_liste[$j]['deb']=$listes[$i]['deb']-$listes[$i]['cred'];
+                $new_liste[$j]['cred']=0;
+                $debit += $new_liste[$j]['deb'];
+            }
+            if($listes[$i]['deb']<$listes[$i]['cred']){
+                $new_liste[$j]=$listes[$i];
+                $new_liste[$j]['deb']=0;
+                $new_liste[$j]['cred']=$listes[$i]['cred']-$listes[$i]['deb'];
+                $credit += $new_liste[$j]['cred'];
+            }
+            $j++;
         }
-    }*/
+        echo count($new_liste);
+        $result['debit']=$debit;
+        $result['credit']=$credit;
+        $result['liste']=$new_liste;
+        if($debit!=$credit){
+            $result['solvable']=false;
+        } else {
+            $result['solvable']=true;
+        }
+        return $result;
+    }
 ?>
--- pages/balance/balance.php
+++ pages/balance/balance.php
@@ -1,6 +1,30 @@
-    <?php if(empty($balance)) { ?>
-        <p class="empty-pan"> Il n'y a pas de solde de balance à inscrire dans </p>
+    <?php
+        require("../inc/balance/fonctions.php");
+        $societe = $_SESSION['id_societe'];
+        $debut = getDebutCompta($societe);
+        $result_grandlivre = getAllSolde($debut,$societe);
+        $balance = treatmentSolde($result_grandlivre);
+        echo count($balance['liste']);
+    ?>
+    <h1>Balance</h1>
+
+    <?php if(empty($result_grandlivre)) { ?>
+        <p class="empty-pan"> Il n'y a pas de solde du grand livre à inscrire dans la balance </p>
     <?php } else { ?>
-    <table border="1">
+    <table border="1" class="ecriture-table">
+        <tr id="ecriture-title">
+            <th class="titre"> Compte </th>
+            <th class="titre"> Designation </th>
+            <th class="titre"> Débit </th>
+            <th class="titre"> Crédit </th>
+        </tr>
+        <?php foreach($result_grandlivre as $ecriture) { ?>
+            <tr id="ecriture_line">
+                <td class="case-compte"> <?php echo $ecriture['numero']; ?> </td>
+                <td class="libelle"> <?php echo $ecriture['designation']; ?> </td>
+                <td class="case-montant"> <?php echo number_format($ecriture['deb'], 0, ' ', ' '); ?> </td>
+                <td class="case-montant"> <?php echo number_format($ecriture['cred'], 0, ' ', ' '); ?> </td>
+            </tr>
         <?php } ?>
     </table>
+    <?php } ?>
