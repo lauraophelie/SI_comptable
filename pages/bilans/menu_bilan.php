7bdb42b469a7fdbc9a96afbf863e781e8b6f6926
Laura Ophélie <lauraophelie1@gmail.com>
2023-04-15 01:33:48 +0200

    modifications

---
+++ pages/bilans/menu_bilan.php
@@ -0,0 +1,34 @@
+<h1 id="main-title"> Etats financiers </h1>
+
+<div class="radio-box">
+    <input type="radio" id="actifs" value="actifs" name="type-bilan"/>
+    <label for="actifs" id="label-type"> Actifs </label>
+
+    <input type="radio" id="passifs" value="passifs" name="type-bilan"/>
+    <label for="passifs" id="label-type"> Passifs </label>
+
+    <input type="radio" id="resultat" value="resultat" name="type-bilan"/>
+    <label for="resultat" id="label-type"> Résultats </label>
+</div>
+
+<center>
+    <p> Veuillez choisir le bilan que vous souhaitez voir </p>
+</center>
+
+<script>
+  const actifsRadio = document.getElementById("actifs");
+  const passifsRadio = document.getElementById("passifs");
+  const resultatRadio = document.getElementById("resultat");
+
+  actifsRadio.addEventListener("click", () => {
+    window.location.href = "./page.php?page=bilans/actifs";
+  });
+
+  passifsRadio.addEventListener("click", () => {
+    window.location.href = "./page.php?page=bilans/passifs";
+  });
+
+  resultatRadio.addEventListener("click", () => {
+    window.location.href = "./page.php?page=bilans/resultat";
+  });
+</script>
---
+++ pages/bilans/passifs.php
@@ -0,0 +1,1 @@
+<h1 id="main-title"> Bilan : Passifs </h1>
