445287a1c6d1b34715f11b415910a47f641cdcb4
Laura Ophélie <lauraophelie1@gmail.com>
2023-04-18 02:01:17 +0200

    passifs

--- inc/bilans/passifs/fonctions.php
+++ inc/bilans/passifs/fonctions.php
@@ -1,3 +1,20 @@
 <?php
+    /*function dbconnect() {
+        $PARAM_hote = 'localhost';
+        $PARAM_nom_bd = 'gestion_compta';
+        $PARAM_utilisateur = 'gestion_compta';
+        $PARAM_mot_passe = 'compta';
+        $PARAM_port = '5432';
+            try {
+                $connexion = new PDO('pgsql:host='.$PARAM_hote.'.;port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
+                return $connexion;
+            }
+            catch(Exception $e) {
+                echo 'Erreur : '.$e-> getMessage().'<br />';
+                echo 'N° : '.$e-> getCode();
+            }
+    }
+    function find_compta($id_societe) {
 
+    }*/
 ?>
--- pages/bilans/passifs.php
+++ pages/bilans/passifs.php
@@ -1,18 +1,27 @@
+<?php
+    require("../inc/ecritures/fonctions.php");
+    $nom_societe = $_SESSION['nom'];
+    $societe = find_societe($nom_societe);
+    $societe_id = $societe['id'];
+    $societe_compta = find_societe_comptabilite($societe_id);
+    $date_fin_exercice = $societe_compta['date_fin_exercice'];
+?>
+
 <h1 id="main-title"> Bilan : Passifs </h1>
 
 <div style="height: 125px"> </div>
 
 <div class="info-societe-box">
-    <h3> Société : </h3>
+    <h3> Société : <?php echo $nom_societe; ?> </h3>
     <h3> Adresse : </h3>
-    <h3> Capital : </h3>
+    <h3> Capital : <?php echo $societe_compta['capital']; ?> Ar </h3>
     <h3> CIF : </h3>
     <h3> STAT : </h3>
 </div>
 
 <div class="bilan-box">
     <h3> Bilan </h3>
-    <h3> EXERCICE CLOS AU : </h3>
+    <h3> EXERCICE CLOS AU : <?php echo $date_fin_exercice; ?> </h3>
     <h3> Unité monétaire : Ariary </h3>
 </div>
 
@@ -30,35 +39,106 @@
         </tr>
         <tr class="passif-line">
             <td> Capital émis </td>
-            <td> 10 </td>
-            <td> 0 </td>
+            <td> 10100 </td>
+            <td> Ar 0 </td>
         </tr>
         <tr class="passif-line">
             <td> Réserves légales </td>
-            <td> 11 </td>
-            <td> 0 </td>
+            <td> 10500 </td>
+            <td> Ar 0 </td>
         </tr>
         <tr class="passif-line">
             <td> Résultat en instance d'affectation </td>
-            <td> 128 </td>
-            <td> 0 </td>
+            <td> 12800 </td>
+            <td> Ar 0 </td>
         </tr>
         <tr class="passif-line">
             <td> Résultat net </td>
-            <td> 120 </td>
-            <td> 0 </td>
+            <td> 12000 </td>
+            <td> Ar 0 </td>
         </tr>
         <tr class="passif-line">
             <td> Autres capitaux propres </td>
-            <td> 11 </td>
-            <td> 0 </td>
+            <td> 11000 </td>
+            <td> Ar 0 </td>
         </tr>
         <tr class="passif-line total-line">
             <th> 
                 <h4 style="text-align:center"> TOTAL DES CAPITAUX PROPRES </h4>
             </th>
             <th> </th>
-            <th> 0 </th>
+            <th> Ar 0 </th>
+        </tr>
+        <tr class="passif-line">
+            <th colspan="4"> 
+                <h4> PASSIFS NON COURANTS </h4>
+            </th>
+        </tr>
+        <tr class="passif-line">
+            <td> Impôts différés </td>
+            <td> 13000 </td>
+            <td> Ar 0 </td>
+        </tr>
+        <tr class="passif-line">
+            <td> Emprunts / dettes à LMT part +1 an </td>
+            <td> 16100 </td>
+            <td> Ar 0 </td>
+        </tr>
+        <tr class="passif-line total-line">
+            <th> 
+                <h4 style="text-align:center"> TOTAL DES PASSIFS NON COURANTS </h4>
+            </th>
+            <th> </th>
+            <th> Ar 0 </th>
+        </tr>
+        <tr class="passif-line">
+            <th colspan="4"> 
+                <h4> PASSIFS COURANTS </h4>
+            </th>
+        </tr>
+        <tr class="passif-line">
+            <td> Emprunts / dettes à LMT part -1 an </td>
+            <td> 16100 </td>
+            <td> Ar 0 </td>
+        </tr>
+        <tr class="passif-line">
+            <td> Dettes courts termes </td>
+            <td> 16500 </td>
+            <td> Ar 0 </td>
+        </tr>
+        <tr class="passif-line">
+            <td> Fournisseurs et comptes rattachés </td>
+            <td> 40000 </td>
+            <td> Ar 0 </td>
+        </tr>
+        <tr class="passif-line">
+            <td> Avance reçues des clients </td>
+            <td> 40000 </td>
+            <td> Ar 0 </td>
+        </tr>
+        <tr class="passif-line">
+            <td> Autres dettes </td>
+            <td> 40000 </td>
+            <td> Ar 0 </td>
+        </tr>
+        <tr class="passif-line">
+            <td> Comptes de trésoreries </td>
+            <td> 50000 </td>
+            <td> Ar 0 </td>
+        </tr>
+        <tr class="passif-line total-line">
+            <th> 
+                <h4 style="text-align:center"> TOTAL DES PASSIFS COURANTS </h4>
+            </th>
+            <th> </th>
+            <th> Ar 0 </th>
+        </tr>
+        <tr class="passif-line total-line">
+            <th> 
+                <h4 style="text-align:center"> TOTAL DES CAPITAUX PROPRES ET PASSIFS </h4>
+            </th>
+            <th> </th>
+            <th> Ar 0 </th>
         </tr>
     </table>
 </div>
