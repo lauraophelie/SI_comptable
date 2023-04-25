e5ae01f75fa44d776803d0ba4525e82a3b6ca7d9
SantatraM <santatryn@gmail.com>
2023-04-25 12:50:25 +0300

    Update script2.sql

--- script2.sql
+++ script2.sql
@@ -1,5 +1,5 @@
 create or replace view solde as
-SELECT DISTINCT numero,designation as libelle,societe, SUM(debit) as total_debit,SUM(credit) as total_credit
+SELECT DISTINCT numero,designation as libelle,societe, SUM(debit) as total_debit,SUM(credit) as total_credit,date_ecriture
 FROM v_balance
-GROUP BY numero,libelle,societe
+GROUP BY numero,libelle,societe,date_ecriture
 ORDER BY numero;
