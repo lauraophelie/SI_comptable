1e42671421692b7580c69eb370475bbb6203e636
Laura Ophélie <lauraophelie1@gmail.com>
2023-04-29 16:27:29 +0200

    affichage

--- pages/ecritures/ecriture.php
+++ pages/ecritures/ecriture.php
@@ -71,8 +71,8 @@
                     <th> </th>
                 </tr>
                 <tr id="input-line">
-                    <td> <input type="text" name="fixe" id="" placeholder="%"> </td>
-                    <td> <input type="text" name="variable" id="" placeholder="%"> </td>
+                    <td> <input type="text" name="fixe" id="fixe" placeholder="%"> </td>
+                    <td> <input type="text" name="variable" id="variable" placeholder="%"> </td>
                     <td>
                         <select name="inc" id="">
                             <option value="inc"> Incorporable </option>
@@ -96,38 +96,39 @@
     <script src="../assets/js/jquery.js"> </script>
     <script src="../assets/js/parsley.js"> </script>
     <script src="./ecritures/js/script.js"> </script>
+    <script src="./ecritures/js/script_2.js"> </script>
 
     <script>
 
         function envoyerEcritures() {
 
-            var societe_nom = document.getElementById("societe_nom").value;
-            var code_journal = document.getElementById("code_journal").value;
-            var rows = [];
+            var societe_nom     = document.getElementById("societe_nom").value;
+            var code_journal    = document.getElementById("code_journal").value;
+            var rows            = [];
 
             $("#table-ecriture tr").each(function(){
                 
-                var date = $(this).find("td:eq(0)").text();
-                var numero_piece = $(this).find("td:eq(1)").text();
-                var cg = $(this).find("td:eq(2)").text();
-                var ct = $(this).find("td:eq(3)").text();
-                var libelle = $(this).find("td:eq(4)").text();
-                var devise = $(this).find("td:eq(5)").text();
-                var montant_devise = $(this).find("td:eq(6)").text();
-                var taux = $(this).find("td:eq(7)").text();
-                var debit = $(this).find("td:eq(8)").text();
-                var credit = $(this).find("td:eq(9)").text();
+                var date            = $(this).find("td:eq(0)").text();
+                var numero_piece    = $(this).find("td:eq(1)").text();
+                var cg              = $(this).find("td:eq(2)").text();
+                var ct              = $(this).find("td:eq(3)").text();
+                var libelle         = $(this).find("td:eq(4)").text();
+                var devise          = $(this).find("td:eq(5)").text();
+                var montant_devise  = $(this).find("td:eq(6)").text();
+                var taux            = $(this).find("td:eq(7)").text();
+                var debit           = $(this).find("td:eq(8)").text();
+                var credit          = $(this).find("td:eq(9)").text();
                 var row = {
-                    date: date,
-                    numero_piece: numero_piece,
-                    cg: cg,
-                    ct: ct,
-                    libelle: libelle,
-                    devise: devise,
+                    date:           date,
+                    numero_piece:   numero_piece,
+                    cg:             cg,
+                    ct:             ct,
+                    libelle:        libelle,
+                    devise:         devise,
                     montant_devise: montant_devise,
-                    taux: taux,
-                    debit: debit,
-                    credit: credit
+                    taux:           taux,
+                    debit:          debit,
+                    credit:         credit
                 };
                 rows.push(row);
             });
@@ -135,9 +136,9 @@
                 type: "GET",
                 url: "../inc/ecritures/traitement_insert.php",
                 data: {
-                    societe_nom: societe_nom,
-                    code_journal: code_journal,
-                    ecritures: rows
+                    societe_nom:    societe_nom,
+                    code_journal:   code_journal,
+                    ecritures:      rows
                 },
                 success: function(response) {
                     alert(response);
