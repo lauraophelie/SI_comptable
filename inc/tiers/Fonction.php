7195f7699b1aba13cef373c69a3e945a6c4797e5
SantatraM <santatryn@gmail.com>
2023-06-10 09:29:02 +0300

    modif_tiers_produit

---
+++ inc/tiers/Fonction.php
@@ -0,0 +1,36 @@
+<?php
+    function db_connect() {
+        $PARAM_hote = 'localhost';
+        $PARAM_nom_bd = 'gestion_compta';
+        $PARAM_utilisateur = 'gestion_compta';
+        $PARAM_mot_passe = 'compta';
+        $PARAM_port = '5432';
+
+        try {
+            $connexion = new PDO('pgsql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
+            return $connexion;
+        } catch(Exception $e) {
+            echo 'Erreur : '.$e-> getMessage().'<br />';
+            echo 'N° : '.$e-> getCode();
+        }   
+    }
+
+    function findAll() {
+        $connexion = db_connect();
+        $sql = "SELECT * from v_tiers ORDER BY numero ASC";
+        $stmt = $connexion->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    function fintById($id) {
+        $connexion = db_connect();
+        $sql = "SELECT * from v_tiers where tiers_id= :id";
+        $stmt = $connexion -> prepare($sql);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $resultat;
+    }
+?>
